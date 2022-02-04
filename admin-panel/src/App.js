973ce4ff391334ce47cafe38f1abc7bf045f9b6d
Shrey <patelshreyu5@gmail.com>
2022-02-04 15:51:03 -0500

    admin panel added

---
+++ admin-panel/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import LandingPage from './components/LandingPage'
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
+import ForgetPasswordPage from './components/ForgetPasswordPage'
+import HomePage from './components/HomePage'
+
+import './App.css'
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={ LandingPage } />
+                    <Route path="/login" component={ LoginPage } />
+                    <Route path="/register" component={ RegisterPage } />
+                    <Route path="/forget-password" component={ ForgetPasswordPage } />
+                    <Route path="/home" component={ HomePage } />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
